d8a517d06ca9a909c3702d31d9cc29f80b8c36ed
djordje <djordje.bigjoe@gmail.com>
2012-11-15 20:22:34 +0100

    - Added library bootstrap and override for email validator

---
+++ config/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @copyright Copyright 2012, Djordje Kovacevic (http://djordjekovacevic.com)
+ * @license   http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+/**
+ * Include validators that override some of Lithium's default validators
+ */
+require dirname(dirname(__DIR__)) . '/validators/overridden.php';
+
+/**
+ * Include validators that doesn't affect Lithium's default validators
+ */
+//require dirname(dirname(__DIR__)) . '/validators/custom.php';
+
+?>
---
+++ validators/overridden.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @copyright Copyright 2012, Djordje Kovacevic (http://djordjekovacevic.com)
+ * @license   http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+use lithium\util\Validator;
+
+/**
+ * - `email`: Check that value is valid email. The available options are
+ *   `'mx'` boolean that enable validator to check if MX DNS record exists and
+ *   `'pattern'` mixed `false` to use `filter_var()` function (default in lithium)
+ *   or regex to check against. By default this filter check against custom regex
+ *   that doesn't match all [RFC 5322](http://tools.ietf.org/html/rfc5322) valid
+ *   emails, but will match against most correct emails, and doesn't check domain
+ *   against MX DNS record. With combinations of this options you can achieve
+ *   enough validations, including lithium's default (`'mx' => false, 'pattern' => false` ).
+ */
+Validator::add('email', function($value, $format, $options) {
+	$defaults = array(
+		'mx' => false,
+		'pattern' => '/^[a-z0-9][a-z0-9_.-]*@[a-z0-9.-]{3,}\.[a-z]{2,4}$/i'
+	);
+	$options += $defaults;
+	$valid = true;
+	switch ($options['pattern']) {
+		case false:
+			$valid = filter_var($value, FILTER_VALIDATE_EMAIL);
+			break;
+		default:
+			$valid = preg_match($options['pattern'], $value);
+			break;
+	}
+	if ($valid && $options['mx'] && function_exists('checkdnsrr')) {
+		$valid = checkdnsrr(end(explode('@', $value)), 'MX');
+	}
+	return $valid;
+});
